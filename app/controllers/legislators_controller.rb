022b12013cdec5b4dcf00dbea40c1df25a522a29
Jacqueline Herrlin <jherrlin@Jacqueline-Herrlins-MacBook.local>
2012-09-02 22:17:49 -0700

    Cleaning up code

--- app/controllers/legislators_controller.rb
+++ app/controllers/legislators_controller.rb
@@ -1,23 +1,16 @@
 class LegislatorsController < ApplicationController
-  # include Kernel
   def index
-    # @legislator = Legislator.search(params[:search])
     @legislator = Legislator.find_by_lastname(params[:lastname])
-    # @legislators = Legislator.find(:all, :conditions => ['lastname LIKE ?', "%#{params[:search]}%"])
   end
   
   def show
     @legislator = Legislator.find_by_lastname(params[:lastname])
-    # @contribution = 
-    # @legislator = Legislator.find_by_lastname("Ellison")
   end
 
   def new
-    # @legislator = Legislator.find_by_lastname(:lastname)
   end
 
   def create 
-    # @legislator = Legislator.find_by_lastname(:lastname)
   end
 
   def edit
@@ -27,11 +20,5 @@ class LegislatorsController < ApplicationController
   end
 
   def destroy
-  end
-  
-  def return_legislators
-    # @legislators = Legislator.order(:lastname).where("lastname like ?", "%#{params[:term]}%")
-    #     audit(@legislators)
-    #     render json: @legislators.map(&:lastname)
   end
 end
--- app/models/legislator.rb
+++ app/models/legislator.rb
@@ -1,22 +1,10 @@
 class Legislator < ActiveRecord::Base
-  attr_accessible :bioguide_id, :firstname, :lastname, :legislative_body, :party, :state, :contributions_attributes
-  has_many :contributions
-  # accepts_nested_attributes_for :contributions
+  attr_accessible :bioguide_id, :firstname, :lastname, :legislative_body, :party, :state
   
   def self.search(search)
     if search
       find(:all, :conditions => ['lastname LIKE ?', "%#{search}%"])
-    # else
-    #   find(:all)
     end
   end
-  
-  # def legislator_lastname
-  #   legislator.lastname if legislator
-  # end
-  # 
-  # def legislator_lastname=(lastname)
-  #   self.legislator = Legislator.find_by_lastname(lastname) unless lastname.blank?
-  # end
 end
 
